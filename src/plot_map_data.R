3ca0546d355b3f300dc34a7a6539f2d1a9061024
aissaelouafi <aissaelouafi1994@gmail.com>
2017-10-15 01:57:15 +0200

    Convert adress to geo cordinates lat and long

---
+++ src/plot_map_data.R
@@ -0,0 +1,22 @@
+library(solr)
+library(ggplot2)
+library(plotly)
+library(httr)
+library(RJSONIO)
+library(jsonlite)
+
+options(scipen=999)
+
+geocodeAdddress <- function(address) {
+  url <- "http://maps.google.com/maps/api/geocode/json?address="
+  url <- URLencode(paste(url, address, "&sensor=false", sep = ""))
+  x <- fromJSON(url, simplify = FALSE)
+  if (x$status == "OK") {
+    out <- c(x$results[[1]]$geometry$location$lng,
+             x$results[[1]]$geometry$location$lat)
+  } else {
+    out <- NA
+  }
+  Sys.sleep(0.2)  # API only allows 5 requests per second
+  out
+}
